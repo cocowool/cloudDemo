4bce2873347d6d52cfdf7102d086c294d940f403
Green Wang <cocowool@qq.com>
2019-07-01 19:37:18 +0800

    add cloud zuul

---
+++ cloud-zuul/src/main/java/com/cloud/zuul/cloudzuul/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.zuul.cloudzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}

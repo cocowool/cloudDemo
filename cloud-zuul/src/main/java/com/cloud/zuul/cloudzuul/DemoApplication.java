02938216b6381d01ccbe03a5654db8d7272a35b7
Green Wang <cocowool@qq.com>
2019-07-01 20:02:23 +0800

    test zuul

--- cloud-zuul/src/main/java/com/cloud/zuul/cloudzuul/DemoApplication.java
+++ cloud-zuul/src/main/java/com/cloud/zuul/cloudzuul/DemoApplication.java
@@ -2,8 +2,10 @@ package com.cloud.zuul.cloudzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class DemoApplication {
 
 	public static void main(String[] args) {
